3a0d73daf6239ef622068e7dbf7a905bc540d9c0
nelsonic <nelson+github@dwyl.io>
2020-04-30 23:24:59 +0100

    attempt to add cache_static_manifest to enable static files #1

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :app, AppWeb.Endpoint,
     transport_options: [socket_opts: [:inet6]]
   ],
   url: [scheme: "https", host: "auth-plug-example.herokuapp.com", port: 443],
-  # cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json"
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
   # ssl: true
