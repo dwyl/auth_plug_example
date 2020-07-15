4a5d315d6af4e8af3dea1ec13d8248258d4e0269
nelsonic <nelson+github@dwyl.io>
2020-07-15 08:43:33 +0100

    tidy up /admin test #5

--- test/app_web/controllers/page_controller_test.exs
+++ test/app_web/controllers/page_controller_test.exs
@@ -6,11 +6,12 @@ defmodule AppWeb.PageControllerTest do
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
 
-  # @tag endpoint: "/admin"
   test "get_session(conn, :jwt)", %{conn: conn} do
     data = %{email: "alice@dwyl.com", givenName: "Alice",
-    picture: "this", auth_provider: "google"}
+      picture: "this", auth_provider: "google"}
+
     jwt = AuthPlug.Token.generate_jwt!(data)
+
     conn =
       conn
       |> put_req_header("authorization", jwt)
@@ -18,6 +19,7 @@ defmodule AppWeb.PageControllerTest do
 
     token = get_session(conn, :jwt)
     {:ok, decoded} = AuthPlug.Token.verify_jwt(token)
+
     assert Map.get(decoded, "email") == "alice@dwyl.com"
     assert html_response(conn, 200) =~ "Welcome Alice!"
   end
