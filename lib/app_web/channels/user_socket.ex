a3de8958efa4722ccfef8378fe61f872f85301a3
SimonLab <simon.labondance@gmail.com>
2021-10-08 11:58:28 +0100

    ignore socket function in test

--- lib/app_web/channels/user_socket.ex
+++ lib/app_web/channels/user_socket.ex
@@ -15,9 +15,11 @@ defmodule AppWeb.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
+  # coveralls-ignore-start
   def connect(_params, socket, _connect_info) do
     {:ok, socket}
   end
+  # coveralls-ignore-stop
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
   #
