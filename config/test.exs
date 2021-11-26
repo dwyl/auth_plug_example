c380bfe51b809545ff210fbe161841806757d5b5
nelsonic <nelson+github@dwyl.com>
2021-11-26 23:48:31 +0000

    remove api_key from config/test.exs to avoid confusion

--- config/test.exs
+++ config/test.exs
@@ -10,6 +10,4 @@ config :app, AppWeb.Endpoint,
 config :logger, level: :warn
 
 config :auth_plug,
-  api_key:
-    "2cfxNaVanCskA5bDFehsfeLL8zyekkhfiNGyAZuLJaEvwGuGwvA/2cfxNa2yvEDA5tHudHR9AMGvzzzZ1j4Y1MXQ7jTQzzAawdxxyfU/authdev.herokuapp.com",
   httpoison_mock: true
